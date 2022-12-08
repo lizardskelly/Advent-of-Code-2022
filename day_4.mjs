ddcb12567f254696b445bc920f45731eca1bf6fb
Liz Meeker <lizmeeker90@gmail.com>
2022-12-08 13:28:42 -0500

    extracted functions

--- day_4.mjs
+++ day_4.mjs
@@ -2,42 +2,40 @@ import { day4 as input } from './inputs.mjs';
 
 // Question: https://adventofcode.com/2022/day/4"
 
+const parseInput = arr => {
+  return arr.map(ranges => {
+    return ranges
+      .split(/,|-/)
+      .map(string => Number(string));
+  });
+}
+
+const parsedInput = parseInput(input);
+
 // Part 1:
 
 const overlapCheck = arr => {
-  return arr
-    .map(ranges => {
-      return ranges
-        .split(/,|-/)
-        .map(string => Number(string));
-    })
-    .reduce((count, ranges) => {
+  return arr.reduce((count, ranges) => {
       if (ranges[0] >= ranges[2] && ranges[1] <= ranges[3]) count++;
       else if (ranges[0] <= ranges[2] && ranges[1] >= ranges[3]) count++;
       return count;
     }, 0);
 }
 
-console.log(overlapCheck(input));
+console.log(overlapCheck(parsedInput));
 
 // Answer: 644
 
 // Part 2:
 
 const overlapCheckPlus = arr => {
-  return arr
-  .map(ranges => {
-    return ranges
-      .split(/,|-/)
-      .map(string => Number(string));
-  })
-  .reduce((count, ranges) => {
+  return arr.reduce((count, ranges) => {
     if (ranges[0] <= ranges[2] && ranges[1] >= ranges[2]) count++;
     else if (ranges[2] <= ranges[0] && ranges[3] >= ranges[0]) count++;
     return count;
   }, 0);
 }
 
-console.log(overlapCheckPlus(input));
+console.log(overlapCheckPlus(parsedInput));
 
 // Answer: 926
